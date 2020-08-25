869bc304c7b013979b1472a39aac9d1eb7b81cd8
Giovanni D'Introno <giovanni.dintrono@gmail.com>
2020-08-25 18:17:25 +0200

    aggiunta addEnvelope

--- src/com/utilities/JdomUtil.java
+++ src/com/utilities/JdomUtil.java
@@ -4,6 +4,7 @@ import org.apache.commons.collections4.IteratorUtils;
 import org.jdom2.Document;
 import org.jdom2.Element;
 import org.jdom2.JDOMException;
+import org.jdom2.Namespace;
 import org.jdom2.filter.ElementFilter;
 import org.jdom2.input.SAXBuilder;
 import org.jdom2.output.Format;
@@ -141,6 +142,38 @@ public class JdomUtil {
 
         return (sResult);
     }
+    public static String addSoapEnvelop(Element sDocIn, Element sHeader, String sNamespacePrefix, String sNamespaceUri){
+
+        Element eDocIn = null, eHeaderIn = null;
+        Element eBody = new Element("Body");
+        Element eSoapEnvelope = new Element("Envelope");
+        eBody.addContent(sDocIn);
+
+        eSoapEnvelope.addContent(sHeader);
+
+        Namespace nsSoap = Namespace.getNamespace("soapenv", "http://schemas.xmlsoap.org/soap/envelope/");
+        sHeader.setNamespace(nsSoap);
+
+        eSoapEnvelope.setNamespace(nsSoap);
+        eBody.setNamespace(nsSoap);
+        eSoapEnvelope.addContent(eBody);
+        if(sNamespaceUri != null)
+        {
+            Namespace ns = Namespace.getNamespace(sNamespacePrefix, sNamespaceUri);
+            eSoapEnvelope.addNamespaceDeclaration(ns);
+        }
+
+        Document docOut = new Document(eSoapEnvelope);
+        return ConvertJDOM(docOut, "utf-8");
+
+    }
+
+    public static String addSoapEnvelop(Element sDocIn, Element sHeader){
+
+        return addSoapEnvelop(sDocIn, sHeader, null, null);
+
+    }
+
 }
 
 
